b03af9f6de8c1cdd5dc63af6816f1ffdcd1ab80e
bird_linux <1224530931@qq.com>
2024-04-14 12:26:06 +0800

    RGB value debug modify

--- lib/evaluators/img_fit.py
+++ lib/evaluators/img_fit.py
@@ -28,7 +28,10 @@ class Evaluator:
         psnr_item = psnr(gt_rgb, pred_rgb, data_range=1.)
         self.psnrs.append(psnr_item)
         save_path = os.path.join(cfg.result_dir, 'vis/res.jpg')
-        imageio.imwrite(save_path, img_utils.horizon_concate(gt_rgb, pred_rgb))
+        # imageio.imwrite(save_path, img_utils.horizon_concate(gt_rgb, pred_rgb))
+        gt_rgb_uint8 = (gt_rgb * 255).astype(np.uint8)
+        pred_rgb_uint8 = (pred_rgb * 255).astype(np.uint8)
+        imageio.imwrite(save_path, img_utils.horizon_concate(gt_rgb_uint8, pred_rgb_uint8))
 
     def summarize(self):
         ret = {}
